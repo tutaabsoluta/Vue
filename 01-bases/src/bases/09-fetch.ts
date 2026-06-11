82fdebe14c17e7c0894f53c4f083fd0fb40a5236
Sergio Duran <538ser@gmail.com>
2026-06-10 18:01:14 -0600

    update fetch file

--- 01-bases/src/bases/09-fetch.ts
+++ 01-bases/src/bases/09-fetch.ts
@@ -1,1 +1,15 @@
 
+
+// const apiKey = process.env.GIPHY_SECRET_KEY;
+const apiKey = process.env.GIPHY_SECRET_KEY;
+const url = `https://api.giphy.com/v1/randomid?api_key=${apiKey}`;
+
+fetch(url)
+    .then((res) => res.json())
+    .then(({ data }) => {
+        console.log(data)
+    })
+    .catch((err) => {
+        console.log(err)
+    })
+
--- 01-bases/src/main.ts
+++ 01-bases/src/main.ts
@@ -1,7 +1,7 @@
-import './style.css'
-import typescriptLogo from './assets/typescript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+// import './style.css'
+// import typescriptLogo from './assets/typescript.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
 import { firstName, lastName } from './bases/01-const-let'
 
 // import './bases/03-arrays'
@@ -15,11 +15,7 @@ import './bases/09-fetch'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${typescriptLogo}" class="framework" alt="TypeScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
+
   <div>
     <h1>Get started</h1>
     <p>Edit <code>src/main.ts</code> and save to test <code>HMR</code></p>
