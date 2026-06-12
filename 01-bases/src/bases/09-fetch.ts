a60199ce53866accc053cd30120c7c3ed5021494
Sergio Duran <538ser@gmail.com>
2026-06-11 18:06:50 -0600

    add types to fetch

--- 01-bases/src/bases/09-fetch.ts
+++ 01-bases/src/bases/09-fetch.ts
@@ -1,13 +1,13 @@
-
+import type { GIFResponse } from "../interfaces/gif.response";
 
 // const apiKey = process.env.GIPHY_SECRET_KEY;
-const apiKey = process.env.GIPHY_SECRET_KEY;
-const url = `https://api.giphy.com/v1/randomid?api_key=${apiKey}`;
+const apiKey = import.meta.env.VITE_GIPHY_SECRET_KEY;
+const url = `https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`;
 
 fetch(url)
     .then((res) => res.json())
-    .then(({ data }) => {
-        console.log(data)
+    .then(( body: GIFResponse ) => {
+        console.log(body.data.images.original.url)
     })
     .catch((err) => {
         console.log(err)
