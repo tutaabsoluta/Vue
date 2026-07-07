8748e9a2a525294b0c4f3bebdaa2352f5b832c13
Sergio Duran <538ser@gmail.com>
2026-07-07 13:27:56 -0600

    add setup method to app.js

--- 02-intro-vue/app.js
+++ 02-intro-vue/app.js
@@ -4,9 +4,22 @@ const { createApp, ref } = Vue;
 
 const app = createApp({
     template: `
-        <h1>Hola Mundo</h1>
-        <p>Desde app.js</p>
-    `
+    <h1>{{ message }}</h1>
+    <p>Desde app.js</p>
+    `,
+    
+    // setup se ejecuta cuando la aplicacion se monta
+    setup() {
+        const message = `Im Batman`;
+
+        // exponemos el message al template
+        return {
+            message
+        }
+    },
+    
+
+    
 })
 
 
