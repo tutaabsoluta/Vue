c4dc7f80c7191a917c1f27755445595d91bc8473
Sergio Duran <538ser@gmail.com>
2026-07-14 16:55:19 -0600

    create new app.vue and remove default folders

--- 03-indecision-app/src/App.vue
+++ 03-indecision-app/src/App.vue
@@ -1,47 +1,14 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
-import TheWelcome from './components/TheWelcome.vue';
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>My App</h1>
 </template>
 
+<script lang="ts" setup></script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+h1 {
+  color: aqua;
 }
 </style>
+
+<!-- Setup en el script es el metodo setup que usamos  -->
+<!-- Scoped solo aplica ese estilo a este componente -->
